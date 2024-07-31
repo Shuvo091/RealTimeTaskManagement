a28644c5c9dc2af4b90a0c70a85127d8c2314eb2
Mehedi Hasan Shuvo <mehedi.shuvo@enosisbd.com>
2024-07-31 20:03:54 +0600

    Add xunit tests

---
+++ RealTimeTaskManagement.Tests/Controllers/PaymentControllers/RazorPayControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeTaskManagement.Payment.RazorPay;
+using RealTimeTaskManagement.Presentation.Controllers.PaymentControllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeTaskManagement.Tests.Controllers
+{
+    public class RazorPayControllerTest
+    {
+        private readonly RazorPayController _controller;
+        public RazorPayControllerTest()
+        {
+            _controller = new RazorPayController();
+        }
+
+        [Fact]
+        public void RazorPayController_Index_ReturnsView()
+        {
+            // Arrange
+
+            // Act
+            var result = _controller.Index();
+
+            //Assert
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void RazorPayController_RazorPay_ReturnsJsonResult_WithOrder()
+        {
+            // Arrange
+            var paymentAmount = "1000";
+
+            // Act
+            var result = _controller.RazorPay(paymentAmount) as JsonResult;
+
+            // Assert
+            result.Value.Should().NotBeNull();
+
+            var response = JsonConvert.DeserializeObject<Response>(JsonConvert.SerializeObject(result.Value));
+            response.Should().NotBeNull();
+            response.Should().BeOfType<Response>();
+        }
+    }
+}
